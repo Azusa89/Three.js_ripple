bb5638e2f7759ceba6f762a7e9512fe0e7e8e4ac
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2023-02-08 08:43:25 +0900

    app set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import "./App.css";
 import * as THREE from "three";
 import { Canvas } from "react-three-fiber";
+import circleImg from "./assets/circle.png";
+import { Suspense } from "react";
+
+function Points() {}
+
+function AnimationCanvas() {
+  return (
+    <Canvas
+      colorManagement={false}
+      camera={{ position: [100, 10, 0], fov: 75 }}
+    >
+      <Points />
+    </Canvas>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="anim"></div>;
+  return (
+    <div className="anim">
+      <Suspense fallvback={<div>Loading...</div>}>
+        <AnimationCanvas />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
