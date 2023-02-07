b2b503790d6b4808d74b008b0b3cb19cf26accb2
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2023-02-08 08:34:17 +0900

    asset load

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import * as THREE from "three";
+import { Canvas } from "react-three-fiber";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return <div className="anim"></div>;
 }
 
 export default App;
